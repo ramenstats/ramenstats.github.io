5e6db387f774b61a129c4e70bf4258f7e435b0f5
ramenstats <ramenghosh13@gmail.com>
2025-11-22 11:56:05 +0530

    Add various enums for team members and categories

---
+++ types/enums.ts
@@ -0,0 +1,52 @@
+export enum TeamMemberCategory {
+    Mentor = 'mentor',
+    Collaborator = 'collaborator',
+    InternationalStudents = 'international-students',
+    Student = 'student',
+}
+
+export enum TeachingArrayFields {
+    Routine = "routine",
+    CourseObjectives = "courseObjectives",
+    programmeObjectives = "programmeObjectives",
+    TeachingLearningProccess = "teachingLearningProccess",
+    Syllabus = "syllabus",
+    ReferenceBooks = "referenceBooks",
+    Prerequisites = "prerequisites",
+    GradingPolicy = "gradingPolicy",
+    OtherResources = "otherResources",
+    Miscellaneous = "miscellaneous",
+}
+
+export enum InternshipArrayFields {
+    MinimumQualification = "minimumQualification",
+    PreferredQualification = "preferredQualification",
+    Prerequisites = "prerequisites",
+    Responsibilities = "responsibilities",
+}
+
+export enum ProfileCategory {
+    InternshipMentor = 'InternshipMentor',
+    CurrentStudent = 'CurrentStudent',
+    PreviousStudent = 'PreviousStudent',
+    MyMentor = 'MyMentor',
+    Collaborator = 'Collaborator',
+    Webteam = 'Webteam',
+}
+
+export enum PublicationCategory {
+    Conference = 'Conference',
+    Journal = 'Journal',
+    Book = 'Book',
+}
+
+export enum AchievementCategory {
+    International = 'International',
+    National = 'National',
+}
+
+export enum ProjectCategory {
+    CG = 'CG',
+    ISRO = 'ISRO',
+    WBDST = 'WBDST',
+}
